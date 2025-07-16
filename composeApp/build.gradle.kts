6a69358fc78eface101a9c7a2c5ce1cb593c8749
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2025-07-16 12:56:21 +0200

    Support JS target for Safari

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,6 +16,11 @@ kotlin {
         binaries.executable()
     }
 
+    js {
+        browser()
+        binaries.executable()
+    }
+
     sourceSets {
         commonMain.dependencies {
             implementation(compose.runtime)
